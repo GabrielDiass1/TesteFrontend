ba99697556189ca5008c6827f7984279b54b8278
GabrielDiass1 <gabriel_dias10@hotmail.com>
2024-05-29 16:05:44 -0300

    .

---
+++ API/APITeste/APITeste/Properties/Models/EFDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+
+namespace APITeste.Models
+{
+    public class EFDBContext : DbContext
+    {
+        public DbSet<Categoria> Categoria { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+    }
+
+}
